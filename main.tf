76d2668e567abab461e0f09c17dfb06790b0523d
Rajpavan123 <140317072+Rajpavan123@users.noreply.github.com>
2023-09-14 11:30:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,18 +11,3 @@ resource "aws_vpc" "my_1011_vpc" {
         Name    = "my_1011_vpc"
     }
 }
-
-resource "aws_securitygroup" "my_1110_sg" {
-    name = "my_sg"
-    vpc_id = "my_1011_vpc"
-
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
-
-}
